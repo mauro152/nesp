93c13efe0da8422ee2f8483de5adad758a13ee4a
Mauro Forte <mauroforte93@gmail.com>
2020-09-04 13:43:04 +0200

    ok

--- public/my/js/login.js
+++ public/my/js/login.js
@@ -1,9 +1,11 @@
 document.getElementById('first').addEventListener('submit', (event) => {
     
-    let password=document.getElementById('password').nodeValue.length;
+    let password=document.getElementById('password').value.length;
     if (password <8) {
+        event.preventDefault();
         console.log('password less than 8');
     } else {
+        event.preventDefault();
         console.log('ciao');
     }
 });
